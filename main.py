d749a188ddaea48f0d986c8d63c9b2913011107d
Finley Jones <finley@finleyjones.co.uk>
2023-08-06 23:24:13 +0100

    added multiprocessing, threads don't communicate

--- main.py
+++ main.py
@@ -1,66 +1,89 @@
 ## Modules ##
 import pygame
+import multiprocessing
 ## Files ##
 import classes
 
-pygame.init()
+def gameLogicProcess(worldPop, environment, settings, worldPopLock, environmentLock, settingsLock):
+    useOvercrowding = settings['useOvercrowding']
 
-displayWidth, displayHeight = 500, 500
+    while settings['running'] == True:
+        with worldPopLock, environmentLock, settingsLock:
+            worldPop.move(environment.gridSize, environment.plotSize)
+            worldPop.reproduce(environment.plotSize, useOvercrowding)
+            settings['running'] = False if worldPop.findSurvivors(environment) == False or settings['running'] == False else True
 
-screen = pygame.display.set_mode([displayWidth, displayHeight], pygame.RESIZABLE)
-running = True
-clock = pygame.time.Clock()
-FPS = 60
-iteration = 0
+def renderProcess(worldPop, environment, settings, worldPopLock, environmentLock, settingsLock):
+    pygame.init()
 
-showEnv, showPop = True, True
-useOvercrowding = True
+    displayWidth, displayHeight = settings['displayWidth'], settings['displayHeight']
 
-environment = classes.Environment(10, 50, (255, 105, 180))
-worldPop = classes.Population(2, ((environment.gridSize//2)*environment.plotSize, (environment.gridSize//2)*environment.plotSize), 0.3, (255, 105, 180), 0.3) # Home in centre
-#worldPop = classes.Population(100, (1, 1), 0.3, (200, 100, 180), 0.5) # Home at 1, 1
+    screen = pygame.display.set_mode([displayWidth, displayHeight], pygame.RESIZABLE)
+    clock = pygame.time.Clock()
+    FPS = settings['FPS']
+    iteration = 0
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                showEnv = not showEnv
-            if event.key == pygame.K_2:
-                showPop = not showPop
-            if event.key == pygame.K_EQUALS:
-                worldPop.resistance += 0.1
-                worldPop.resistance = round(worldPop.resistance, 1)
-            if event.key == pygame.K_MINUS:
-                worldPop.resistance -= 0.1
-                worldPop.resistance = round(worldPop.resistance, 1)
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.VIDEORESIZE:
-            displayWidth, displayHeight = event.w, event.h
-            surface = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE)
+    showEnv, showPop = settings['showEnv'], settings['showPop']
 
-    iteration += 1
-    screen.fill((0, 0, 0))
+    with worldPopLock, environmentLock, settingsLock:
+        while settings['running']:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        showEnv = not showEnv
+                    if event.key == pygame.K_2:
+                        showPop = not showPop
+                    if event.key == pygame.K_EQUALS:
+                        worldPop.resistance += 0.1
+                        worldPop.resistance = round(worldPop.resistance, 1)
+                    if event.key == pygame.K_MINUS:
+                        worldPop.resistance -= 0.1
+                        worldPop.resistance = round(worldPop.resistance, 1)
+                if event.type == pygame.QUIT:
+                    settings['running'] = False
+                if event.type == pygame.VIDEORESIZE:
+                    displayWidth, displayHeight = event.w, event.h
+                    screen = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE)
 
-    worldPop.move(environment.gridSize, environment.plotSize)
-    worldPop.reproduce(environment.plotSize, useOvercrowding)
-    running = False if worldPop.findSurvivors(environment) == False or running == False else True
+            iteration += 1
+            screen.fill((0, 0, 0))
 
-    #print("x: ", pygame.mouse.get_pos()[0]//environment.plotSize, "   y: ", pygame.mouse.get_pos()[1]//environment.plotSize)
-    print(f'''
-\n----------\n
-Iteration: {iteration}
-Pop Size: {len(worldPop.pop)}
-Resistance: {worldPop.resistance}
-''')
+            #print("x: ", pygame.mouse.get_pos()[0]//environment.plotSize, "   y: ", pygame.mouse.get_pos()[1]//environment.plotSize)
+            print(f'\n----------\n\nIteration: {iteration}\nPop Size: {len(worldPop.pop)}\nResistance: {worldPop.resistance}\n')
 
-    if showEnv:
-        environment.display(screen, displayHeight, displayWidth)
-    if showPop:
-        worldPop.display(screen, displayHeight, displayWidth, environment.gridSize, environment.plotSize)
+            if showEnv:
+                environment.display(screen, displayHeight, displayWidth)
+            if showPop:
+                worldPop.display(screen, displayHeight, displayWidth, environment.gridSize, environment.plotSize)
 
-    pygame.display.flip()
-    clock.tick(FPS)
+            pygame.display.flip()
+            clock.tick(FPS)
 
-pygame.quit()
-print('Done!')
+        pygame.quit()
+
+if __name__ == "__main__":
+    environment = classes.Environment(10, 50, (255, 105, 180))
+    worldPop = classes.Population(2, ((environment.gridSize//2)*environment.plotSize, (environment.gridSize//2)*environment.plotSize), 0.3, (255, 105, 180), 0.3) # Home in centre
+    #worldPop = classes.Population(100, (1, 1), 0.3, (200, 100, 180), 0.5) # Home at 1, 1
+    settings = {
+        'displayWidth': 500,
+        'displayHeight': 500,
+        'FPS': 60,
+        'showEnv': True,
+        'showPop': True,
+        'useOvercrowding': True,
+        'running': True
+    }
+    
+    environmentLock = multiprocessing.Lock()
+    worldPopLock = multiprocessing.Lock()
+    settingsLock = multiprocessing.Lock()
+
+    gameLogicProc = multiprocessing.Process(target=gameLogicProcess, args=(worldPop, environment, settings, worldPopLock, environmentLock, settingsLock))
+    renderingProc = multiprocessing.Process(target=renderProcess, args=(worldPop, environment, settings, worldPopLock, environmentLock, settingsLock))
+
+    gameLogicProc.start()
+    renderingProc.start()
+
+    gameLogicProc.join()
+    renderingProc.join()
